e93a0f6beaaacda9d0e6e354c38348e9c5d5b260
momoscode <aramax2000@hotmail.com>
2013-08-19 09:13:48 -0400

    test

---
+++ DMNews/FileUtils.vb
@@ -0,0 +1,31 @@
+﻿'Aram Peres, le 18 août 2013
+'Information sur cette classe: Outils de fichiers utiliés pour le programme, manipulations risquées.
+Public Class FileUtils
+
+    Public Shared appdata As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+    Private Shared Sub verifyDMApps()
+        If Not System.IO.Directory.Exists(appdata & strings.DMAppsDir) Then
+            MsgBox(strings.loadingError, MsgBoxStyle.Critical)
+            End
+        End If
+
+    End Sub
+    Private Shared Sub verifyDMNews()
+        If Not System.IO.Directory.Exists(appdata & strings.DMNewsDir) Then
+            MsgBox(strings.loadingError, MsgBoxStyle.Critical)
+            End
+        End If
+    End Sub
+    Private Shared Sub verifyVersion()
+        If Not System.IO.Directory.Exists(appdata & strings.DMNewsDir & strings.Version) Then
+            MsgBox(strings.loadingError, MsgBoxStyle.Critical)
+            End
+        End If
+    End Sub
+    Shared Sub verifyDirs()
+        'Verifie si les dossiers principaux existent. Si non, un message d'erreur est envoyé.
+        verifyDMApps()
+        verifyDMNews()
+        verifyVersion()
+    End Sub
+End Class
